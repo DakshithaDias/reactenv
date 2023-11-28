820a6a534a85c4bd8a26532212599ad0a3c0188a
Dakshitha Dias <dakshithadias@gmail.com>
2023-11-28 23:11:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const environ = process.env.REACT_APP_ENVIRONMENT || 'development';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Dakshitha</h1>
+          <h2>Environment : {environ}</h2>
       </header>
     </div>
   );
